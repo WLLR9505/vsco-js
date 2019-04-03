7a661833e515ee07a91717d3d20ad36d76a0accd
Weuller Jr. Souza <weuller9505@live.com>
2019-04-02 21:17:49 -0300

    Implementação inicial

--- README.md
+++ README.md
@@ -1,1 +1,29 @@
 # vsco-js
+
+Ferramenta para download de imagem do site vsco.co utilizando web scraping
+
+---
+
+## Dependências
+- cheerio
+- nightmare
+
+---
+
+## Uso
+
+Execute o comando abaixo substituindo *imageURL* pela url da imagem desejada  
+
+`node index imageURL`
+
+Será gerada uma pasta './imgs' com a imagem salva
+
+---
+
+## Como funciona?
+
+- A url passada como argumento é processada pelo **nightmare** que retorna a página  
+- A pagina é processada pelo **cheerio** procurando pela classe *.css-6j68gn*  
+- A URL para a imagem comprimida fica no *src* desta classe  
+- É feita uma conversão para uma nova URL que redirecione para a imagem original  
+- A nova URL é processado pela função *saveImage* que faz sua solicitação **https** e salva a imagem retornada na pasta *imgs*
---
+++ index.js
@@ -0,0 +1,53 @@
+var https = require('https');
+const fs = require("fs");
+const cheerio = require("cheerio");
+var Nightmare = require('Nightmare');
+nightmare = new Nightmare({
+    show: false
+});
+
+var src = new String;
+const [,, ... args] = process.argv;
+
+main(args[0]);
+
+
+function main(url) {
+    nightmare
+    .goto(url)
+    .wait(200)
+    .evaluate(() => {
+        console.log(document.body.innerHTML);
+        return document.body.innerHTML;
+    })
+    .end()
+    .then((body) => {
+        var $ = cheerio.load(body);
+        src = $('.css-6j68gn').attr("src");
+        //tenta fazer substituição válida
+        src = src.replace('//im.vsco.co/1/', 'https://image.vsco.co/1/');
+        src = src.replace('//im.vsco.co/aws-us-west-2/', 'https://image-aws-us-west-2.vsco.co/');
+        //utilizado para nomear a imagem
+        let endName = src.lastIndexOf('.jpg');
+        let startname =  src.lastIndexOf('/');
+        saveImage(src, './imgs', src.slice(startname + 1, endName) + '.jpg')
+    })
+}
+
+function saveImage(url, folder, imgName) {
+    if (!fs.existsSync(folder)){    //se a pasta não existir, cria
+        fs.mkdirSync(folder);
+    }
+    var file = fs.createWriteStream(folder + '/' + imgName);
+    try {
+        var request = https.get(url, (response) => {
+            console.log('SAVED!');
+            response.pipe(file);
+        });
+        return 0;
+    } catch (e) {
+        console.log('ERROR');
+        console.log(url);
+        return 1;
+    }
+}
